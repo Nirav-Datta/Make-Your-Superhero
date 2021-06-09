d69a027634e858f771a8b519969d7764250220a4
Nirav Datta <78023371+Nirav-Datta@users.noreply.github.com>
2021-06-09 13:56:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var keyPressed;
 var canvas = new fabric.Canvas('myCanvas');
 var block_width = 30;
 var block_height = 30;
@@ -28,4 +29,44 @@ function new_image(get_image){
         });
 canvas.add(block_image_object);
     });
+}
+window.addEventListener("keydown", my_keydown);
+function my_keydown(e){
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if(e.shiftKey == true && keyPressed == '80'){
+        console.log("P and Shift pressed together");
+        block_image_width = block_image_width + 10;
+        block_image_height = block_image_height + 10;
+        document.getElementById("current_width").innerHTML=block_image_width;
+        document.getElementById("current_height").innerHTML=block_image_height;
+    }
+    if(e.shiftKey == true && keyPressed == '77'){
+        console.log("M and Shift pressed together");
+        block_image_width = block_image_width - 10;
+        block_image_height = block_image_height - 10;
+        document.getElementById("current_width").innerHTML=block_image_width;
+        document.getElementById("current_height").innerHTML=block_image_height;
+    }
+    if(keyPressed == '70') { new_image('spiderman_face.png'); console.log("F");}
+    if(keyPressed == '66') { new_image('ironman_body.png'); console.log("B");}
+    if(keyPressed == '76') { new_image('hulk_legs.png'); console.log("L");}
+    if(keyPressed == '82') { new_image('thor_right_hand.png'); console.log("R");}
+    if(keyPressed == '72') { new_image('ironman_left_hand.png'); console.log("H");}
+    if(keyPressed == '38'){
+        up();
+        console.log("up");
+    }
+    if(keyPressed == '40'){
+        down();
+        console.log("down");
+    }
+    if(keyPressed == '37'){
+        left();
+        console.log("left");
+    }
+    if(keyPressed == '39'){
+        right();
+        console.log("right");
+    }
 }
